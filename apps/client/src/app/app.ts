2c92042f3b754f8791cdb6d47dcba3366a35ade9
Łukasz Fereniec <lukaszfereniec@MacBookAir.home>
2026-01-04 19:18:42 +0100

    implemented sketch

---
+++ apps/client/src/app/app.ts
@@ -0,0 +1,13 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+
+@Component({
+  imports: [RouterModule],
+  selector: 'app-root',
+  templateUrl: './app.html',
+  styleUrl: './app.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class App {
+  protected title = 'client';
+}
